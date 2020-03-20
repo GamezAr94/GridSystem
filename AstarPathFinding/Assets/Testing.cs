5258fef7ce85435eb30de8a7fe47787e260078aa
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-03-20 13:49:41 -0700

    Creating scripts
    adding scripts to create a grid with numerical values, you can click on each grid cell to obtain the value or to change the value

---
+++ AstarPathFinding/Assets/Testing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamez;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+    private void Start()
+    {
+        grid = new Grid(4, 2, 10f, new Vector3(20,0));
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.LogError(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
+}
